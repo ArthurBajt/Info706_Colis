30eec7140e28125b97958d59c3fc88b9b4e3b421
Arthur <arthur.bajt@gmail.com>
2021-10-05 10:30:52 +0200

    Servelet helloworld

---
+++ ProjetJava/LivraisonColis/src/main/java/com/example/livraisoncolis/ServletHello.java
@@ -0,0 +1,20 @@
+package com.example.livraisoncolis;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ServletHello", value = "/ServletHello")
+public class ServletHello extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().write("Hello Bébou !");
+        response.getWriter().close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
