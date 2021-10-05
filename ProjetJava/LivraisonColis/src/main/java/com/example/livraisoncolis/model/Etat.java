88dbabde8b870e02a933c8e75e832f056e26461c
Luxem <luxem192@gmail.com>
2021-10-05 11:17:35 +0200

    Création enum et classe
    Création de la classe Position et de l'énumaration Etat

---
+++ ProjetJava/LivraisonColis/src/main/java/com/example/livraisoncolis/model/Etat.java
@@ -0,0 +1,9 @@
+package com.example.livraisoncolis.model;
+
+public enum Etat {
+	ENREGISTREMENT,
+	EN_ATTENTE,
+	EN_ACHEMINEMENT,
+	BLOQUE,
+	LIVRE
+}
---
+++ ProjetJava/LivraisonColis/src/main/java/com/example/livraisoncolis/model/Position.java
@@ -0,0 +1,61 @@
+package com.example.livraisoncolis.model;
+
+public class Position {
+
+	double latitude;
+	double longitude;
+	String emplacement;
+	Etat etat;
+	
+	public Position(double lat, double longi, String emp) {
+		this.latitude=lat;
+		this.longitude=longi;
+		this.emplacement=emp;
+		this.etat=Etat.ENREGISTREMENT;
+		
+	}
+
+	
+	
+	@Override
+	public String toString() {
+		return "Position [latitude=" + latitude + ", longitude=" + longitude + ", emplacement=" + emplacement
+				+ ", etat=" + etat + "]";
+	}
+
+
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getEmplacement() {
+		return emplacement;
+	}
+
+	public void setEmplacement(String emplacement) {
+		this.emplacement = emplacement;
+	}
+
+	public Etat getEtat() {
+		return etat;
+	}
+
+	public void setEtat(Etat etat) {
+		this.etat = etat;
+	}
+	
+	
+}
