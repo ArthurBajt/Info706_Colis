7603dfb885b04140de1ca6221b274d80b99c4024
Luxem <luxem192@gmail.com>
2021-10-22 10:05:25 +0200

    push fonction find et suppression truc useless

--- ProjetJava/LivraisonColis/src/main/java/com/example/livraisoncolis/controller/ColisEJB.java
+++ ProjetJava/LivraisonColis/src/main/java/com/example/livraisoncolis/controller/ColisEJB.java
@@ -18,15 +18,20 @@ public class ColisEJB {
 	public Colis addColis(long id, double poid, double valeur, String origine, String destination, Position achemeniment) {
 		Colis c = new Colis(id, poid, valeur, origine, destination, achemeniment);
 		em.persist(c);
-		// em.getTransaction().commit(); // a verifier mais je crois qu on doit commit quand on ajoute une donnee.
 		
 		return c;
 	}
 
 
+	public Colis findColis(long id){
+		Colis c = em.find(Colis.class, id);
+		return c;
+	}
+	
     public void removeColis(long id){
 		Colis c = em.find(Colis.class, id);
 		em.remove(c);
-		em.getTransaction().commit();
     }
+    
+    
 }
